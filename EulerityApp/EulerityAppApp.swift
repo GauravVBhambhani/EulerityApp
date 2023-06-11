90d4ea28342e97bf13c5a04922fd0b76464e60f1
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-06-11 18:19:12 -0400

    Initial Commit

---
+++ EulerityApp/EulerityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityAppApp.swift
+//  EulerityApp
+//
+//  Created by Gaurav Bhambhani on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EulerityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
